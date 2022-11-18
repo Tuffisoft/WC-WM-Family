f4fc2346f7a484e9a5cf2a4f421ce4520c3f41b9
Fi <vancetti@googlemail.com>
2022-11-18 09:07:23 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+
+
+export default function App() {
+
+  return (
+    <>
+      <div className="card w-200 bg-base border-4">
+        <figure className="px-200 pt-10 border-solid">
+          <img src="https://placeimg.com/400/225/arch" alt="Shoes" className="rounded-xl" />
+          <div className="card-body items-center text-center">
+            <h2 className="card-title">Amélie</h2>
+            <p>Mexico</p>
+          </div>
+          <img src="https://placeimg.com/400/225/arch" alt="Shoes" className="rounded-xl" />
+      </figure>
+      <div className="card-body items-center text-center">
+            <h2 className="card-title">Amélie</h2>
+            <p>Mexico</p>
+          </div>
+</div>
+      
+    </>
+  )
+}
+
+
